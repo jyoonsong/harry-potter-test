53e4d9540b4cf5874a0c8ce8e9b797701ecd6516
Jaeyoon Song <jyo3on@gmail.com>
2021-05-24 17:18:58 -0400

    start gatsby repo

---
+++ src/pages/question.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const Question = () => (
+  <Layout>
+    <Seo title="Page two" />
+    <h1>Hi from the second page</h1>
+    <p>Welcome to page 2</p>
+    <Link to="/">다시 하기</Link>
+  </Layout>
+)
+
+export default Question
