c6929fe6dad402831cc89be5a321273e5b81ed38
Jaeyoon Song <jyo3on@gmail.com>
2021-05-26 20:45:57 -0400

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,17 @@
 import * as React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
 import Seo from "../components/seo"
 
+
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404: 페이지가 없습니다</h1>
+    <img src="/images/reducto.jpg" alt="reducto spell" />
+    <p>누군가 ㄹㄹㄹㄹㄹ리덕토(reducto) 주문으로 이 페이지를 고장낸 것 같습니다. 다른 페이지를 이용해주세요.</p>
+    <Link to="/">홈으로 가기</Link>
   </Layout>
 )
 
